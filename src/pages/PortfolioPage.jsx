3b03d363ea2abb279d6c3aef5f4909177f082ee3
JakeHowdeshell <135374279+JakeHowdeshell@users.noreply.github.com>
2023-11-03 13:03:06 -0500

    added an array of projects, and mapped across them to display their information as a card

--- src/pages/PortfolioPage.jsx
+++ src/pages/PortfolioPage.jsx
@@ -1,19 +1,62 @@
-export default function Portfolio() {
-    return (
-      <div>
-        <h1>About Page</h1>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed neque
-          velit, lobortis ut magna varius, blandit rhoncus sem. Morbi lacinia nisi
-          ac dui fermentum, sed luctus urna tincidunt. Etiam ut feugiat ex. Cras
-          non risus mi. Curabitur mattis rutrum ipsum, ut aliquet urna imperdiet
-          ac. Sed nec nulla aliquam, bibendum odio eget, vestibulum tortor. Cras
-          rutrum ligula in tincidunt commodo. Morbi sit amet mollis orci, in
-          tristique ex. Donec nec ornare elit. Donec blandit est sed risus feugiat
-          porttitor. Vestibulum molestie hendrerit massa non consequat. Vestibulum
-          vitae lorem tortor. In elementum ultricies tempus. Interdum et malesuada
-          fames ac ante ipsum primis in faucibus.
-        </p>
-      </div>
-    );
+import Project from "../components/UI/Project";
+
+const projects = [
+  {
+    id: 1,
+    name: "PWA Text Editor",
+    url: "https://extra-progressive-text-editor-9dd627b180ce.herokuapp.com/",
+    image: "./images/textEditor.png",
+    alt: "Text editor application deployed on heroku",
+    gitHub: "https://github.com/JakeHowdeshell/pwa-text-editor.git",
+  },
+  {
+    id: 2,
+    name: "Express.js Note Taker",
+    url: "https://great-note-taker-f60a4a0b9185.herokuapp.com/",
+    image: "./images/noteTaker.png",
+    alt: "Note taker application deployed on heroku",
+    gitHub: "https://github.com/JakeHowdeshell/express.js-note-taker.git",
+  },
+  {
+    id: 3,
+    name: "MVC Tech Blog",
+    url: "https://original-tech-blog-ecc03b1e97dd.herokuapp.com/",
+    image: "./images/techBlog.png",
+    alt: "Tech blog application deployed on heroku",
+    gitHub: "https://github.com/JakeHowdeshell/mvc-tech-blog.git",
+  },
+  {
+    id: 4,
+    name: "Apparel Database",
+    url: "https://ut-bros-apparel-0bcd7da3cb4b.herokuapp.com/login",
+    image: "./images/apperalDatabase.png",
+    alt: "Apparel database application deployed on heroku",
+    gitHub: "https://github.com/cprademacher/apparel-database.git",
+  },
+  {
+    id: 5,
+    name: "Quote Generator",
+    url: "https://r-warden.github.io/Quote-Generator/",
+    image: "./images/quoteGenerator.png",
+    alt: "Quote generator application deployed on heroku",
+    gitHub: "https://github.com/r-warden/Quote-Generator.git",
+  },
+  {
+    id: 6,
+    name: "another App",
+    url: "this test",
+    image: "",
+    alt: "",
+    gitHub: "",
   }
+];
+export default function Portfolio() {
+  return (
+    <div>
+      <h4>Portfolio</h4>
+      {projects.map((project) => (
+        <Project key={project.id} project={project} />
+      ))}
+    </div>
+  );
+}
