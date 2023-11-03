cbb8701dcdc7aa0c9999343e68a7a25006a57a0a
JakeHowdeshell <135374279+JakeHowdeshell@users.noreply.github.com>
2023-11-03 12:51:41 -0500

    added two paragraphs about me and addded a placeholder for my picture

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -1,18 +1,26 @@
 export default function About() {
     return (
       <div>
-        <h1>About Page</h1>
+        {/* need to add am image of myself */}
+        <h4>About Me</h4>
         <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed neque
-          velit, lobortis ut magna varius, blandit rhoncus sem. Morbi lacinia nisi
-          ac dui fermentum, sed luctus urna tincidunt. Etiam ut feugiat ex. Cras
-          non risus mi. Curabitur mattis rutrum ipsum, ut aliquet urna imperdiet
-          ac. Sed nec nulla aliquam, bibendum odio eget, vestibulum tortor. Cras
-          rutrum ligula in tincidunt commodo. Morbi sit amet mollis orci, in
-          tristique ex. Donec nec ornare elit. Donec blandit est sed risus feugiat
-          porttitor. Vestibulum molestie hendrerit massa non consequat. Vestibulum
-          vitae lorem tortor. In elementum ultricies tempus. Interdum et malesuada
-          fames ac ante ipsum primis in faucibus.
+          Hello, my name is Jake Howdeshell welcome to my coding portfolio page.
+          A few fun facts about me are that I am task oriented, enjoy breaking
+          my work into accomplishable sections to keep my motivation high! I am
+          an avid climber and enjoy being in the outdoors soaking up the sun and
+          fresh air. Lastly I am a big fan of live music and try to catch as
+          many live shows as I can, typically hearing the band for the first
+          time a couple days before the concert.
+        </p>
+        <p>
+          My professional background covers everything from sales, and customer
+          service, to manual labor, to platform management. In the last few
+          years I have been more exposed to the tech side of things and found a
+          lot of enjoyment in the problem solving aspect and the limitmess
+          potential to create and recreate helpful tools, applications, and
+          websites. Please check out my current work, contact information, and
+          resume provided below. I am currently on the hunt for new and exciting
+          work opportunities in the tech field!
         </p>
       </div>
     );
