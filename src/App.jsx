08fb8763575cb9d2b0d348b06520e179460ccbb4
JakeHowdeshell <135374279+JakeHowdeshell@users.noreply.github.com>
2023-11-03 12:49:40 -0500

    imported the css and addded header section with an h1 element

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,18 @@
 import { Outlet } from 'react-router-dom';
 import Nav from './components/NavTabs';
+import './App.css';
 
 function App() {
   // The Outlet component will conditionally swap between the different pages according to the URL
   return (
     <>
+    <header className="title"><h1>JakeHowdeshell</h1>
       <Nav />
+      </header>
       <main className="mx-3">
         <Outlet />
       </main>
+      <footer></footer>
     </>
   );
 }
