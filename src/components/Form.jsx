64fa9d609be111fc35566dfc02cd6da29e68c90d
JakeHowdeshell <135374279+JakeHowdeshell@users.noreply.github.com>
2023-11-03 13:04:55 -0500

    created a for component for the contact me page

---
+++ src/components/Form.jsx
@@ -0,0 +1,82 @@
+import { useState } from "react";
+
+import { validateEmail } from "../utils/helpers";
+
+function Form() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
+
+  const handleInputChange = (e) => {
+    const { target } = e;
+    const inputType = target.name;
+    const inputValue = target.value;
+
+    if (inputType === "name") {
+      setName(inputValue);
+    } else if (inputType === "email") {
+      setEmail(inputValue);
+    } else {
+      setMessage(inputValue);
+    }
+  };
+
+  const handleFormSubmit = (e) => {
+    e.preventDefault();
+
+    if (!name) {
+      setErrorMessage("Please provide your name");
+      return;
+    }
+    if (!validateEmail(email)) {
+      setErrorMessage("Please provide a valid email address");
+      return;
+    }
+    alert(`Thank you ${name} I will get back to you as soon as possible!`);
+   
+    // need to have it send an email
+
+    setName("");
+    setEmail("");
+    setMessage("");
+  };
+  return (
+    <div className="container text-center">
+      <form className="form" onSubmit={handleFormSubmit}>
+      <h6>Name:</h6>
+        <input
+          value={name}
+          name="name"
+          onChange={handleInputChange}
+          type="text"
+          placeholder="name"
+        />
+        <h6>Email:</h6>
+        <input
+          value={email}
+          name="email"
+          onChange={handleInputChange}
+          type="email"
+          placeholder="email"
+        />
+        <h6>Message:</h6>
+        <input
+          value={message}
+          name="message"
+          onChange={handleInputChange}
+          type="text"
+          placeholder="Add message here ..."
+        />
+        <button type="submit">Submit</button>
+      </form>
+      {errorMessage && (
+        <div>
+          <p className="error-text">{errorMessage}</p>
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default Form;
