a2f9a6c6fc025e8c54cd3ee8347c39cd54f5944c
JakeHowdeshell <135374279+JakeHowdeshell@users.noreply.github.com>
2023-11-06 17:18:15 -0600

    added the images and links for the footer

--- src/components/UI/Footer.jsx
+++ src/components/UI/Footer.jsx
@@ -1,4 +1,30 @@
-
 export default function Footer({}) {
-    return (<div></div>);
-  }
+  return (
+    <div>
+      <a href="https://github.com/JakeHowdeshell">
+        <img
+          width="48"
+          height="48"
+          src="https://img.icons8.com/external-tanah-basah-glyph-tanah-basah/96/external-github-social-media-tanah-basah-glyph-tanah-basah.png"
+          alt="external-github-social-media-tanah-basah-glyph-tanah-basah"
+        />
+      </a>
+      <a href="https://www.linkedin.com/in/jacob-howdeshell-2607a3b7">
+        <img
+          width="48"
+          height="48"
+          src="https://img.icons8.com/external-tanah-basah-basic-outline-tanah-basah/48/external-linkedin-social-media-tanah-basah-basic-outline-tanah-basah.png"
+          alt="external-linkedin-social-media-tanah-basah-basic-outline-tanah-basah"
+        />
+      </a>
+      <a href="https://www.instagram.com/jakehowdy/?hl=en">
+        <img
+          width="48"
+          height="48"
+          src="https://img.icons8.com/material-outlined/48/instagram-new--v1.png"
+          alt="instagram-new--v1"
+        />
+      </a>
+    </div>
+  );
+}
