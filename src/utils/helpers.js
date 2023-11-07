14fa0b4bc819ee2801553d6d681226e5a12c5f68
JakeHowdeshell <135374279+JakeHowdeshell@users.noreply.github.com>
2023-11-07 14:32:39 -0600

    added comments on the functionality

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -1,3 +1,4 @@
+// uses the regex to validate the email address provided
 export function validateEmail(email) {
     const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     return re.test(String(email).toLowerCase());
